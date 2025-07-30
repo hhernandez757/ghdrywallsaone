cffcc8e0231c8bfc58a81b1f17e86b0ae921b539
hhernandez757 <hhernandez757@gmail.com>
2025-07-30 07:10:24 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,3 @@
+import OnePage from "./OnePage";
+// import MultiPage from "./MultiPage";
+export default OnePage;
---
+++ src/OnePage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function OnePage() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>GH Drywall - One Page Site</h1>
+      <p>This is the one-page scroll version placeholder.</p>
+    </div>
+  );
+}
